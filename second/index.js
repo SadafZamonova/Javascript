84ebdb75e87fe7057dd4c2e63b94fbd8f5121168
SadafZamonova <84777664+SadafZamonova@users.noreply.github.com>
2021-09-07 15:26:25 +0500

    калькулятор на 4 действия

---
+++ second/index.js
@@ -0,0 +1,23 @@
+function ShowMessageplus() {
+    let a = prompt(`первое число?`);
+    let b = prompt(`второе число?`);
+    alert(Number(a) + Number(b));
+    }
+
+    function ShowMessageminus() {
+        let a = prompt(`первое число?`);
+        let b = prompt(`второе число?`);
+        alert(a - b);
+        }
+
+        function ShowMessagemultiply() {
+            let a = prompt(`первое число?`);
+            let b = prompt(`второе число?`);
+            alert(a * b);
+            }
+
+            function ShowMessagedivide() {
+                let a = prompt(`первое число?`);
+                let b = prompt(`второе число?`);
+                alert(a / b);
+                }
